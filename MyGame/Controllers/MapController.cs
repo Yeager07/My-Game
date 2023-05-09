26d577a1746a98c4410ea67d3a0bc66127187750
Yeager07 <127008112+Yeager07@users.noreply.github.com>
2023-05-09 16:31:10 +0500

    Начал добавлять стены

--- MyGame/Controllers/MapController.cs
+++ MyGame/Controllers/MapController.cs
@@ -11,8 +11,8 @@ namespace MyGame.Controllers
     public static class MapController
     {
         // Величины квадратов
-        public static int cellSizeX = 50;
-        public static int cellSizeY = 50;
+        public static int cellSizeX = 51;
+        public static int cellSizeY = 51;
 
 
         public static int[,] map;
@@ -25,13 +25,15 @@ namespace MyGame.Controllers
         public static void Init()
         {
             map = GetMap();
-            imgMap = new Bitmap(@"..\..\..\Sprites\Back1.png");
+            imgMap = new Bitmap(@"..\..\..\Sprites\Back.png");
         }
 
         public static int[,] GetMap()
         {
             return new int[,]{
                 { 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 },
+                { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 },
+                { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 },
                 { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 },
                 { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 },
                 { 0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0 },
@@ -54,16 +56,21 @@ namespace MyGame.Controllers
 
         public static void DrawMap(Graphics g)
         {
-            cellLimitX = imgMap.Size.Width / cellSizeX;
-            cellLimitY = imgMap.Size.Height / cellSizeY;
+            cellLimitX = GetWidth() / cellSizeX;
+            cellLimitY = GetHeight() / cellSizeY;
 
-            for (int i = 0; i < cellLimitX; i++)
+            for (int i = 0; i < cellLimitY; i++)
             {
-                for (int j = 0; j < cellLimitY; j++)
+                for (int j = 0; j < cellLimitX; j++)
                 {
-                    g.DrawImage(imgMap,
-                        new Rectangle(new Point(cellSizeX * i, cellSizeY * j), new Size(cellSizeX, cellSizeY)),
-                        cellSizeX * i, cellSizeY * j,
+                    if (map[i, j] == 0)
+                    {
+                        g.DrawImage(new Bitmap(@"..\..\..\Sprites\umen.png"), new Rectangle(new Point(cellSizeX * j, cellSizeY * i), new Size(cellSizeX, cellSizeY)), 0, 0, 59, 59, GraphicsUnit.Pixel);
+                    }
+                    else
+                        g.DrawImage(imgMap,
+                        new Rectangle(new Point(cellSizeX * j, cellSizeY * i), new Size(cellSizeX, cellSizeY)),
+                        cellSizeX * j, cellSizeY * i,
                         cellSizeX, cellSizeY,
                         GraphicsUnit.Pixel);
                 }
